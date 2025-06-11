c12a7915f735407fba53e6f1bfdacfb2a6a3b2f6
AidanLam06 <aidanlam02@gmail.com>
2025-06-11 17:18:15 -0400

    Update yara_check.py
    forgot to remove thor handling from yara_check.py after removing the .yar file

--- yara_check.py
+++ yara_check.py
@@ -28,7 +28,5 @@ def match_diagnosis(matches):
     for i in matches:
         if "crime_ransom" in i:
             print("Ransomware was detected within this file. Disconnect from wifi and run antivirus scan immediately")
-        elif "thor_inverse_matches" in i:
-            print("This file contains suspicious code. Run antivirus scan and upload to VirusTotal file scanner for a more precise diagnosis")
         else:
             print("this file contains code that is sometimes seen in malware. Upload the file to a more reputable scanner like VirusTotal for a more in-depth analysis of its contents.")
